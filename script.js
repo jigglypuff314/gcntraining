d602fc6e331d7df511d401e1640493f202af0d0c
jigglypuff314 <67601208+jigglypuff314@users.noreply.github.com>
2024-08-25 13:25:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-console.log("GCN Next Button");
-
-console.log("Checking whether next button is active...");
-var nextbutton = document.getElementsByClassName("next next-active");
-
-setInterval (function() {
-    console.log("Checking...");
-    nextbutton = document.getElementsByClassName("next next-active");
-    if(nextbutton.length == 1){
-        console.log("Clicking on the Next Button.");
-        document.getElementsByClassName("next next-active")[0].click();
-    }
-}, 10000);
+if (window.location.href.startsWith("https://site.gcntraining.com/")) {
+    console.log("GCN Next Button");
+    console.log("Checking whether next button is active...");
+    var nextbutton = document.getElementsByClassName("next next-active");
+    setInterval (function() {
+        console.log("Checking...");
+        nextbutton = document.getElementsByClassName("next next-active");
+        if(nextbutton.length == 1){
+            console.log("Clicking on the Next Button.");
+            document.getElementsByClassName("next next-active")[0].click();
+        }
+    }, 10000);
+}
