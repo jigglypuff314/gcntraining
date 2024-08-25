06114be5d3a31053f3cbe4f164239655dd586b37
jigglypuff314 <67601208+jigglypuff314@users.noreply.github.com>
2024-08-25 13:19:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('This is a popup!');
+console.log("GCN Next Button");
+
+console.log("Checking whether next button is active...");
+var nextbutton = document.getElementsByClassName("next next-active");
+
+setInterval (function() {
+    console.log("Checking...");
+    nextbutton = document.getElementsByClassName("next next-active");
+    if(nextbutton.length == 1){
+        console.log("Clicking on the Next Button.");
+        document.getElementsByClassName("next next-active")[0].click();
+    }
+}, 10000);
